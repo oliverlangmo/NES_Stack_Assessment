d06b8aa9a2ddce64744400054bf982de519858eb
oliverlangmo <oliver.langmo@gmail.com>
2016-06-17 17:02:01 -0500

    kind of works

---
+++ public/scripts/FE-app.js
@@ -0,0 +1,65 @@
+$(document).ready(function(){
+  $('#sendAnimal').on('click', function(){
+    getAnimals();
+    console.log('js and jquery sourced!!');
+   var newAnimal = $('#animalIn').val();
+     var newZooAnimal={
+       'newAnimal': newAnimal
+       };
+   $.ajax({
+    type: 'POST',
+    url: '/addAnimal',
+    data: newZooAnimal
+});
+});
+var getAnimals = function(){
+  $.ajax({
+  type:'GET',
+  url: '/getAnimals',
+  success: function(data){
+    console.log('get Animals click got ' + data);
+    showAnimals(data);
+      }
+    });
+  };
+function showAnimals (animals){
+  console.log(animals);
+  for (var i = 0; i < animals.length; i++) {
+    var animalReturn = "<p>" + animals[i].animal_type + ", quantity: " + animals[i].animal_quantity + "</p>";
+   $('#displayDiv').append(animalReturn);
+                }
+          }
+      });
+
+
+// Create a new table in a new database to track animals in our zoo. It should include an
+// auto-incrementing primary key, a column for the type of animal as a string or varchar,
+// and a number to keep track of how many of this animal type we have.
+//
+// A database.txt should be included with your project which will include the name of
+// the database as well as the names of the tables and columns as well as the commands
+// you used to create your database and tables.
+//
+// Create a Node/Express server app. This app should actively listen for requests on port 3000.
+// It should be able to serve back our static files (index.html, any js or css files)
+// as well as handle incoming requests to any routes. Include a routing module to use with
+// your Ajax requests.
+//
+// Our application needs to be able to input new animals and display current animals.
+// This will require Ajax requests to send data to the server and Ajax requests to retrieve data
+// from the server.
+//
+// When a new kind of animal is entered from the user, use the randomNumber function
+// below to get a number between 1 and 100. Use this number as your number of animals value
+// in the database for this entry.
+//
+// Random number function: function randomNumber(min, max){ return Math.floor(Math.random() * (1 + max - min) + min); }
+// This function could be wrapped in a custom module (hint hint).
+//
+// The app should display all animals listed in our database when it loads and after each animal group
+// is added.
+//
+// Turn In Assessment
+//
+// Turn in your assessment through primeacademy.io/student. Include a link to your Github
+// repo with your code. Remember to include a text file with your table create SQL.
---
+++ server/app.js
@@ -0,0 +1,41 @@
+var express =require('express');
+var app = express();
+var path =require('path');
+var bodyParser=require('body-parser');
+var pg=require('pg');
+var urlencodedParser=bodyParser.urlencoded({extended: false});
+var connectionString = 'postgres://localhost:5432/zoo_keeper';
+var animalNum = require('../modules/animalNum');
+
+
+app.listen(3000, 'localhost', function(){
+  console.log('server is listening on 3000');
+});
+
+app.use(express.static('public'));
+
+app.get('/', function(req,res){
+  console.log('in base url');
+  res.sendFile(path.resolve('views/index.html'));
+});
+app.post('/addAnimal', urlencodedParser, function(req, res){
+  var quantity = animalNum(1,100);
+  console.log( 'in POST addNew: ' + req.body.newAnimal + " quantity: " + quantity );
+      pg.connect( connectionString, function( err, client, done ){
+        if( err ) console.log( err );
+        else client.query( 'INSERT INTO animals_in_zoo ( animal_type, animal_quantity ) VALUES ($1, $2)', [ req.body.newAnimal, quantity] );
+      });
+});
+app.get('/getAnimals', function(req, res){
+console.log('/get animals');
+var results=[];
+pg.connect( connectionString, function(err, client, done){
+  var query = client.query('SELECT * from animals_in_zoo');
+  query.on('row', function(row){
+    results.push(row);
+  });
+  query.on('end', function() {
+  return res.json(results);
+        }); // end onEnd
+});
+});
